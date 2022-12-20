cc28c4ae6699ba2532128d53b5c2857dd1e3c6ed
Siwagon2546 <netevgjqw@gmail.com>
2022-12-20 17:09:52 +0700

    lab8_3.cpp complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if ( x>65 and x<=90)
+	{
+		return x-1;
+	}
+	else if(x==65)
+	{
+		return 'Z';
+	}
+	return '0';
 }
 
 int main(){
